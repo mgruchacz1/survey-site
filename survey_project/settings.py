f2e25949c1ab94ba5927501a96982165239a84b1
Matthew Gruchacz <mcgruchacz77@gmail.com>
2020-08-03 22:11:55 -0700

    project configuration

--- survey_project/settings.py
+++ survey_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import environ
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,18 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# reading .env file
-environ.Env.read_env(env_file=configuration.env)
-
 # SECURITY WARNING: keep the secret key used in production secret!
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-SECRET_KEY = env('SECRET_KEY')
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
